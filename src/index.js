a39dcda4148c1c58a0fa7654c1d761348ac76113
sandaruny <sandaru.ny@gmail.com>
2016-12-11 16:38:43 +0530

    Initial

---
+++ src/index.js
@@ -0,0 +1,28 @@
+var polymer = require('polymer');
+
+
+
+
+// (function(scope){
+//
+//   function Receptors(){
+//
+//   };
+//
+//   function DNA(){
+//     arguments.splice().forEach(d){
+//
+//     }
+//   };
+//
+//
+//
+//
+//   var App = scope.App || {};
+//   App.Recetors = Receptors;
+//   App.DNA = DNA;
+//   App.Recetors = Receptors;
+//   App.Recetors = Receptors;
+//
+//
+// })(window)
